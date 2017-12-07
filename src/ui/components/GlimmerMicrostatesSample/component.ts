6d366bb104f81f5fdf068b349a48463c93b9d9fd
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-12-07 22:17:43 +0530

    Comment broken code

--- src/ui/components/GlimmerMicrostatesSample/component.ts
+++ src/ui/components/GlimmerMicrostatesSample/component.ts
@@ -1,6 +1,7 @@
 import Component from "@glimmer/component";
-import microstates, * as MS from "microstates";
+// import microstates, * as MS from "microstates";
 
 export default class GlimmerMicrostatesSample extends Component {
-  name = microstates(MS.String, "World").state;
+  // name = microstates(MS.String, "World").state;
+  name = "Siva";
 }
